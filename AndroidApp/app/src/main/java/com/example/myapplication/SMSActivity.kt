7e0a6dfe70329af9be60af2c2e9502c135da7e7d
John Murray <jomurray@umass.edu>
2020-11-09 00:34:35 -0500

    Reorganizing, uploading android application v1

---
+++ AndroidApp/app/src/main/java/com/example/myapplication/SMSActivity.kt
@@ -0,0 +1,93 @@
+package com.example.myapplication
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.telephony.SmsManager
+import android.view.MenuItem
+import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+
+class SMSActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val REQUEST_CODE_NEW_FEATURE_PERMISSIONS = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sms)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val hasSMSPermission = checkSelfPermission(Manifest.permission.SEND_SMS)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val permissionstorequest: MutableList<String> = mutableListOf()
+            if (hasSMSPermission != PackageManager.PERMISSION_GRANTED) {
+                permissionstorequest.add(Manifest.permission.SEND_SMS)
+            }
+            if(permissionstorequest.isNotEmpty()) {
+                requestPermissions(permissionstorequest.toTypedArray(), REQUEST_CODE_NEW_FEATURE_PERMISSIONS)
+            }
+        }
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+
+        val sendSMSButton: Button = findViewById(R.id.sms_button)
+        sendSMSButton.setOnClickListener{
+            val SMSMan:SmsManager = SmsManager.getDefault()
+            SMSMan.sendTextMessage("14136825761", null, "EMERGENCY! Shiver-Ring has detected a dangerous situation. \n Please check on ", null,null)
+        }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_calender -> {
+                // Handle the camera action
+            }
+            R.id.nav_bluetooth -> {
+                val bluetoothIntent = Intent(applicationContext, BluetoothActivity::class.java)
+                startActivity(bluetoothIntent)
+            }
+            R.id.nav_sms -> {
+                // Do nothing
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+}
