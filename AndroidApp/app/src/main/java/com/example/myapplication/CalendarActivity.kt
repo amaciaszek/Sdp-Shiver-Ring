e359569a6c2bec71002b2036a56863d5d18981f7
cloughman <54954252+cloughman@users.noreply.github.com>
2020-11-12 20:21:44 -0500

    Added the beginnings of the calendar activity

---
+++ AndroidApp/app/src/main/java/com/example/myapplication/CalendarActivity.kt
@@ -0,0 +1,81 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class CalendarActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val REQUEST_CODE_NEW_FEATURE_PERMISSIONS = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_Calendar)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val permissionstorequest: MutableList<String> = mutableListOf()
+
+            if(permissionstorequest.isNotEmpty()) {
+                requestPermissions(permissionstorequest.toTypedArray(), REQUEST_CODE_NEW_FEATURE_PERMISSIONS)
+            }
+        }
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_calender -> {
+                // Do nothing
+            }
+            R.id.nav_bluetooth -> {
+                val bluetoothIntent = Intent(applicationContext, BluetoothActivity::class.java)
+                startActivity(bluetoothIntent)
+            }
+            R.id.nav_sms -> {
+                val smsIntent = Intent(applicationContext, SMSActivity::class.java)
+                startActivity(smsIntent)
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+}
